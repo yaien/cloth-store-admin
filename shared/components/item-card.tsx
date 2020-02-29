d56621df3483f8c0169455a807ce213aa3d3f976
Stevenson Marquez <stevensonmarquez@MacBook-Pro-de-Stevenson.local>
2020-02-28 22:10:14 -0500

    list item cards

--- shared/components/item-card.tsx
+++ shared/components/item-card.tsx
@@ -1,21 +1,34 @@
 import { FC } from "react";
 import { Item } from "../../lib/store";
-import { Card, CardTitle, CardBody } from "reactstrap";
+import {
+  Card,
+  CardTitle,
+  CardBody,
+  CardText,
+  CardSubtitle,
+  CardImg
+} from "reactstrap";
 
 export interface ItemProps {
-  item: Item
+  item: Item;
 }
 
 export const ItemCard: FC<ItemProps> = ({ item }) => {
   return (
     <Card>
+      <CardImg
+        top
+        width="100%"
+        src="https://reactstrap.github.io/assets/318x180.svg"
+        alt="Card image cap"
+      />
       <CardBody>
-        <CardTitle>
-          {item.name}
-        </CardTitle>
+        <CardTitle>{item.name}</CardTitle>
+        <CardSubtitle>${item.price.toLocaleString()}</CardSubtitle>
+        <CardText>{item.description}</CardText>
       </CardBody>
     </Card>
-  )
-}
+  );
+};
 
-export default ItemCard
+export default ItemCard;
